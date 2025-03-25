5f8e26769ad92cc9d6c7ff01892ad351154fc263
Le Xuan Tho <tho02052004@gmail.com>
2025-03-26 03:58:35 +0700

    DeleteUpdate

--- SQLQuery.sql
+++
@@ -1,8 +0,0 @@
-Use FlowerShop
-Go
-select*from Users
-
-INSERT INTO Users ( username, email, phone, role, password) VALUES
-( 'ThoLe', 'tho@example.com', '1234567890', 'Customer', 'password1'),
-( 'LeTho', 'le@example.com', '0987654321', 'Customer', 'password2'),
-( 'ThoAdmin', 'admin@example.com', '1122334455', 'Owner', 'adminpass');
